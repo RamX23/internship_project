126c6253643be4fb2a1f00da063935f50016da98
RamX23 <112005895+RamX23@users.noreply.github.com>
2024-12-22 15:25:57 +0530

    first commit

---
+++ backend/middlewares/authadmin.js
@@ -0,0 +1,7 @@
+// Middleware to check if the user is an admin
+const isAdmin = (req, res, next) => {
+    if (req.user && req.user.role === 'admin') {
+        return next();
+    }
+    return res.status(403).json({ message: 'Unauthorized access' });
+};
